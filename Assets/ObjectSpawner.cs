48681509493b7246ffe8b077b4de9b4a30715c13
developdata <isk9@hotmail.com>
2021-11-01 17:24:10 +0000

    added clickable surface on detection adding cube

---
+++ Assets/ObjectSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    private PlacementIndicator PlacementIndicator;
+
+    void Start()
+    {
+        PlacementIndicator = FindObjectOfType<PlacementIndicator>();
+
+    }
+
+    
+    void Update()
+    {
+        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began) {
+            {
+                GameObject obj = Instantiate(objectToSpawn, PlacementIndicator.transform.position, PlacementIndicator.transform.rotation);
+            }
+        }
+    }
+}
